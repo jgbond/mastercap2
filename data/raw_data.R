aaeb7caf9490b16b4642b5b28836f928aa53dc5e
jgbond <jgbond@gmail.com>
2020-07-07 18:56:19 +0800

    new file

---
+++ data/raw_data.R
@@ -0,0 +1,8 @@
+#' Earthquake data from the US Geological Survey
+#'
+#' A dataset containing global earthquake data. Source is the
+#' US Geological Survey.
+#'
+#' @format A Tibble with 6195 observations and 47 variables
+#'
+#' @source https://www.ngdc.noaa.gov/nndc/struts/form?t=101650&s=1&d=1
